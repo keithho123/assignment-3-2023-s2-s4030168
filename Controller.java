4a2ee26e489952e16e2d294a457c915c4e21a5e3
s4030168 <s4030168@student.rmit.edu.au>
2023-09-27 18:41:59 +1000

    added Controller functions

--- Controller.java
+++ Controller.java
@@ -6,4 +6,48 @@ public class Controller {
      * Empty constructor
      */
     public Controller(){}
+
+    /**
+     * Function to publish a new course
+     *
+     * @return String containing username and password
+     */
+    public String publishCourse(Course course){
+        String credentials = "";
+        // Publishes the course to the webpage
+        // Generates new credentials
+
+        return credentials;
+    }
+
+    /**
+     * Function to get the course information required
+     *
+     * @return String appending all information
+     */
+    public String enterCourseInfo(){
+        String info = "";
+
+        // Prompt user to enter information + append to info
+
+        return info;
+    }
+
+    /**
+     * Function to print error message when payment of fee isn't successful
+     */
+    public void invalidPaymentError(){
+        System.out.println("ERROR. Invalid payment. Operation Cancelled");
+    }
+
+    /**
+     * Function to create a new course.
+     *
+     * @param user who wants to create course (might be an instructor or not)
+     * @return User after creating course (Instructor if not failure)
+     */
+    public User createCourse(User user){
+        return new User();
+    }
 }
+
