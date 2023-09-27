8bb85562356182b0834d96b19716e5a10755d91c
s4030168 <s4030168@student.rmit.edu.au>
2023-09-27 19:07:50 +1000

    added functions in RegistrationFee

---
+++ RegistrationFee.java
@@ -0,0 +1,56 @@
+/**
+ * RegistrationFee class to pay course's fee
+ */
+public class RegistrationFee {
+
+    /**
+     * Method to send invoice to the user who paid the fee
+     */
+    public void sendInvoice(Invoice invoice, User user){
+       // Send invoice to user's email
+    }
+
+    /**
+     * Method to actually pay the fee
+     *
+     * @return boolean indicating if payment was successful
+     */
+    public boolean verifyPayment(String credentials, String paymentMethod, float fee){
+        boolean success = false;
+        // Attempt to charge designated amount
+        return success;
+    }
+
+    /**
+     * Method to get the payment information required
+     *
+     * @return String indicating desired information (credentials)
+     */
+    public String getPaymentInfo(){
+        String info = "";
+        // Prompt user to enter information
+        return info;
+    }
+
+    /**
+     * Method to select payment method
+     *
+     * @return String indicating desired payment method
+     */
+    public String selectPaymentMethod(){
+        String paymentMethod = "";
+        // Prompt user to decide payment
+        return paymentMethod;
+    }
+
+    /**
+     * Method to publish a new course
+     *
+     * @param fee indicating amount to be paid
+     * @param user to make the payment
+     * @return boolean indicating if payment was successful
+     */
+    public boolean pay(float fee, User user){
+
+    }
+}
