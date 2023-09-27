26154a44112a9c3d73d63aab8d8fa6a2731beb04
s4030168 <s4030168@student.rmit.edu.au>
2023-09-27 19:49:41 +1000

    added main workflow

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+/**
+ * Main class of the program
+ */
 public class Main {
     public static void main(String[] args) {
         // Create controller
         Controller c = new Controller();
+
+        // Create user for interactions with the system
+        User user = new User();
+
+        // Attempt to create a course
+        // newUser might be the same as user or a new Instructor (if the creation was successful)
+        User newUser = c.createCourse(user);
+
     }
 }
