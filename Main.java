f809a245efa3a4910399201fcd30df46a7e1a29b
s4030168 <s4030168@student.rmit.edu.au>
2023-09-28 10:28:19 +1000

    last comments

--- Main.java
+++ Main.java
@@ -9,9 +9,13 @@ public class Main {
         // Create user for interactions with the system
         User user = new User();
 
+        //Sub-scenario 1: register a course
         // Attempt to create a course
         // newUser might be the same as old user or a new Instructor (if the creation was successful)
         User newUser = c.createCourse(user);
+
+
+        //Sub-scenario 2: enquiry
 
     }
 }
